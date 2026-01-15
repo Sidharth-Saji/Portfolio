4d45247c5cdc20e759a594d95a4cd7ecb0a577e0
Sidharth Saji <sidharthsaji2003@gmail.com>
2026-01-15 12:12:12 +0530

    Blogs Added

--- js/blog-search.js
+++ js/blog-search.js
@@ -10,3 +10,20 @@ function filterBlogs() {
       : "none";
   }
 }
+
+function showPage(pageNumber) {
+  // Hide all blog posts
+  $('.blog-post').hide();
+
+  // Show selected page
+  $('.page-' + pageNumber).show();
+
+  // Update active pagination state
+  $('.pagination li').removeClass('active');
+  $('.pagination li:nth-child(' + pageNumber + ')').addClass('active');
+}
+
+// Default: show page 1 on load
+$(document).ready(function () {
+  showPage(1);
+});
